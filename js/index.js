72ea011f909eea3522a858ac21daf7cab9e8feee
TinaKovalova <kovalevatkv@gmail.com>
2026-09-26 18:15:38 +0300

    slider-section

--- js/index.js
+++ js/index.js
@@ -25,12 +25,14 @@ document.addEventListener("DOMContentLoaded", () => {
        );
      });
     
-    const featuredSwiper = new Swiper(".featured__slider.swiper", {
+    const featuredSwiper = new Swiper("#featured-slider", {
       slidesPerView: "auto",
       centerInsufficientSlides: true,
       navigation: {
-        nextEl: ".featured__slider-button-next",
-        prevEl: ".featured__slider-button-prev",
+        nextEl:
+          ".featured__slider-controls .slider-controls__slider-button-next",
+        prevEl:
+          ".featured__slider-controls .slider-controls__slider-button-prev",
       },
       breakpoints: {
         320: {
@@ -76,6 +78,53 @@ document.addEventListener("DOMContentLoaded", () => {
       bookingElement.classList.toggle("active");
     }
   })
+
+  const tourDates = document.querySelectorAll(".booking-form__input[type='date']");
+  if (tourDates.length > 0) {
+    tourDates.forEach(
+      (input) => (input.value = new Date().toISOString().split("T")[0]),
+    );
+  } 
+
+  const relatedTodaySwiper = new Swiper("#related-today-slider", {
+    slidesPerView: "auto",
+    centerInsufficientSlides: true,
+    navigation: {
+      nextEl: ".related-today .slider-controls__slider-button-next",
+      prevEl: ".related-today .slider-controls__slider-button-prev",
+    },
+    breakpoints: {
+      320: {
+        spaceBetween: 10,
+      },
+      480: {
+        spaceBetween: 15,
+      },
+      992: {
+        spaceBetween: 30,
+      },
+    },
+  });
+  const relatedSwiper = new Swiper("#related-slider", {
+    slidesPerView: "auto",
+    centerInsufficientSlides: true,
+    navigation: {
+      nextEl: ".related .slider-controls__slider-button-next",
+      prevEl: ".related .slider-controls__slider-button-prev",
+    },
+    breakpoints: {
+      320: {
+        spaceBetween: 10,
+      },
+      480: {
+        spaceBetween: 15,
+      },
+      992: {
+        spaceBetween: 30,
+      },
+    },
+  });
+  
   
 })
 
